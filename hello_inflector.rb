f14c1c36fcb615f17bd1ef4732588b7275d293a0
Ferdi Ramdhon <ferdirn@gmail.com>
2015-05-12 09:19:30 +0700

    hello inflector

---
+++ hello_inflector.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'active_support/all'
+
+puts "#{1.ordinalize} place!"
+puts "#{2.ordinalize} place!"
+puts "#{3.ordinalize} place!"
+puts "#{5.ordinalize} place!"
+puts "#{10.ordinalize} place!"
+
